92b2c23b585b899a2b03441e1ce8bec73cc8ea03
DukeLurkin <durkin.lukas@gmail.com>
2025-09-11 16:46:20 -0400

    added background to light mode with verticle& horizontal lines as well as dots. Also added orange radial background image

--- dark-light-toggle/src/components/Hero.jsx
+++ dark-light-toggle/src/components/Hero.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="isolate">
+      <div className="absolute inset-0 -z-10">
+        <div className="absolute top-0 -left-1/4 w-1/2 h-1/2 bg-amber-400 dark:bg-amber-500 rounded-full blur-3xl opacity-30">
+        </div>
+      </div>
+      <div className="container mx-auto px-6 py-20 md:py-24 lg:py-28 xl:py-32">
+        <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-12 xl:gap-16">
+          <div className="flex-1 space-y-6 lg:space-y-7">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
